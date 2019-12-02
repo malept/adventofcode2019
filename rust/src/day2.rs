a386cc14f89efe649723fa440ffc46f453b9cbfd
Mark Lee <adventofcode@lazymalevolence.com>
2019-12-02 10:44:37 -0800

    Use nomenclature defined in day 2, part 2

--- rust/src/day2.rs
+++ rust/src/day2.rs
@@ -3,35 +3,35 @@ enum Operation {
     Multiply,
 }
 
-pub fn execute_intcode(instructions: &mut Vec<u32>) {
-    let mut position = 0;
-    while position < instructions.len() {
-        let opcode = instructions[position];
+pub fn execute_intcode(memory: &mut Vec<u32>) {
+    let mut pointer = 0;
+    while pointer < memory.len() {
+        let opcode = memory[pointer];
         match opcode {
-            1 => execute_operation(instructions, position, Operation::Add),
-            2 => execute_operation(instructions, position, Operation::Multiply),
+            1 => execute_instruction(memory, pointer, Operation::Add),
+            2 => execute_instruction(memory, pointer, Operation::Multiply),
             99 => break,
             _ => panic!("Unknown opcode: {:?}", opcode),
         }
 
-        position += 4;
+        pointer += 4;
     }
 }
 
-fn execute_operation(instructions: &mut Vec<u32>, position: usize, operation: Operation) {
-    let first_input_position = instructions[position + 1] as usize;
-    let second_input_position = instructions[position + 2] as usize;
-    let output_position = instructions[position + 3] as usize;
+fn execute_instruction(memory: &mut Vec<u32>, pointer: usize, operation: Operation) {
+    let first_input_address = memory[pointer + 1] as usize;
+    let second_input_address = memory[pointer + 2] as usize;
+    let output_address = memory[pointer + 3] as usize;
 
-    let operand1 = instructions[first_input_position];
-    let operand2 = instructions[second_input_position];
+    let operand1 = memory[first_input_address];
+    let operand2 = memory[second_input_address];
 
     let result = match operation {
         Operation::Add => operand1 + operand2,
         Operation::Multiply => operand1 * operand2,
     };
 
-    instructions[output_position] = result;
+    memory[output_address] = result;
 }
 
 #[cfg(test)]
@@ -40,27 +40,27 @@ mod tests {
 
     #[test]
     fn test_execute_intcode() {
-        let mut instructions = vec![1, 9, 10, 3, 2, 3, 11, 0, 99, 30, 40, 50];
+        let mut memory = vec![1, 9, 10, 3, 2, 3, 11, 0, 99, 30, 40, 50];
         assert_intcode_executed(
-            &mut instructions,
+            &mut memory,
             vec![3500, 9, 10, 70, 2, 3, 11, 0, 99, 30, 40, 50],
         );
 
-        instructions = vec![1, 0, 0, 0, 99];
-        assert_intcode_executed(&mut instructions, vec![2, 0, 0, 0, 99]);
+        memory = vec![1, 0, 0, 0, 99];
+        assert_intcode_executed(&mut memory, vec![2, 0, 0, 0, 99]);
 
-        instructions = vec![2, 3, 0, 3, 99];
-        assert_intcode_executed(&mut instructions, vec![2, 3, 0, 6, 99]);
+        memory = vec![2, 3, 0, 3, 99];
+        assert_intcode_executed(&mut memory, vec![2, 3, 0, 6, 99]);
 
-        instructions = vec![2, 4, 4, 5, 99, 0];
-        assert_intcode_executed(&mut instructions, vec![2, 4, 4, 5, 99, 9801]);
+        memory = vec![2, 4, 4, 5, 99, 0];
+        assert_intcode_executed(&mut memory, vec![2, 4, 4, 5, 99, 9801]);
 
-        instructions = vec![1, 1, 1, 4, 99, 5, 6, 0, 99];
-        assert_intcode_executed(&mut instructions, vec![30, 1, 1, 4, 2, 5, 6, 0, 99]);
+        memory = vec![1, 1, 1, 4, 99, 5, 6, 0, 99];
+        assert_intcode_executed(&mut memory, vec![30, 1, 1, 4, 2, 5, 6, 0, 99]);
     }
 
-    fn assert_intcode_executed(instructions: &mut Vec<u32>, expected: Vec<u32>) {
-        execute_intcode(instructions);
-        assert_eq!(expected.as_slice(), instructions.as_slice());
+    fn assert_intcode_executed(memory: &mut Vec<u32>, expected: Vec<u32>) {
+        execute_intcode(memory);
+        assert_eq!(expected.as_slice(), memory.as_slice());
     }
 }
