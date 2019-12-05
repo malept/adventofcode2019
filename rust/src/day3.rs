f5b58b6f0349aae7ee7fe1debc06507d4d5dae5f
Mark Lee <adventofcode@lazymalevolence.com>
2019-12-05 00:26:12 -0800

    Advent of Code 2019, day 3 (part 2)

--- rust/src/day3.rs
+++ rust/src/day3.rs
@@ -31,7 +31,7 @@ impl FromStr for Direction {
 }
 
 impl Direction {
-    pub fn from_line(line: String) -> Vec<Direction> {
+    pub fn from_line(line: &str) -> Vec<Direction> {
         line.split(",")
             .map(|serialized| serialized.parse().expect("Cannot parse direction"))
             .collect()
@@ -46,195 +46,194 @@ struct Point {
 
 impl Ord for Point {
     fn cmp(&self, other: &Self) -> Ordering {
-        calculate_manhattan_distance(self).cmp(&calculate_manhattan_distance(other))
+        self.manhattan_distance().cmp(&other.manhattan_distance())
     }
 }
 
-struct Wire(Vec<Point>);
+impl Point {
+    pub fn manhattan_distance(&self) -> i32 {
+        self.x + self.y
+    }
+}
 
-fn calculate_wire(directions: Vec<Direction>) -> Wire {
-    let mut points = vec![];
+#[derive(Debug)]
+pub struct Wire(Vec<Point>);
 
-    let mut position = Point::default();
+impl Wire {
+    pub fn from_directions(directions: Vec<Direction>) -> Wire {
+        let mut points = vec![];
+        let mut position = Point::default();
 
-    for direction in directions {
-        let mut line = match direction {
-            Direction::Up(distance) => {
-                points.reserve(distance as usize);
-                (0..distance)
-                    .map(|_i| {
-                        position.x += 1;
-                        position.clone()
-                    })
-                    .collect()
-            }
-            Direction::Down(distance) => {
-                points.reserve(distance as usize);
-                (0..distance)
-                    .map(|_i| {
-                        position.x -= 1;
-                        position.clone()
-                    })
-                    .collect()
-            }
-            Direction::Left(distance) => {
-                points.reserve(distance as usize);
-                (0..distance)
-                    .map(|_i| {
-                        position.y -= 1;
-                        position.clone()
-                    })
-                    .collect()
-            }
-            Direction::Right(distance) => {
-                points.reserve(distance as usize);
-                (0..distance)
-                    .map(|_i| {
-                        position.y += 1;
-                        position.clone()
-                    })
-                    .collect()
-            }
-        };
+        for direction in directions {
+            let mut line = match direction {
+                Direction::Up(distance) => {
+                    points.reserve(distance as usize);
+                    (0..distance)
+                        .map(|_i| {
+                            position.x += 1;
+                            position.clone()
+                        })
+                        .collect()
+                }
+                Direction::Down(distance) => {
+                    points.reserve(distance as usize);
+                    (0..distance)
+                        .map(|_i| {
+                            position.x -= 1;
+                            position.clone()
+                        })
+                        .collect()
+                }
+                Direction::Left(distance) => {
+                    points.reserve(distance as usize);
+                    (0..distance)
+                        .map(|_i| {
+                            position.y -= 1;
+                            position.clone()
+                        })
+                        .collect()
+                }
+                Direction::Right(distance) => {
+                    points.reserve(distance as usize);
+                    (0..distance)
+                        .map(|_i| {
+                            position.y += 1;
+                            position.clone()
+                        })
+                        .collect()
+                }
+            };
 
-        points.append(&mut line);
+            points.append(&mut line);
+        }
+
+        Wire(points)
     }
 
-    Wire(points)
-}
+    pub fn from_directions_string(serialized: &str) -> Wire {
+        Wire::from_directions(Direction::from_line(serialized))
+    }
 
-fn find_wire_intersections(wire1: Wire, wire2: Wire) -> HashSet<Point> {
-    let points1: HashSet<Point> = wire1.0.into_iter().collect();
-    let points2: HashSet<Point> = wire2.0.into_iter().collect();
+    fn intersections(&self, other: &Wire) -> HashSet<Point> {
+        let self_set: HashSet<Point> = self.0.clone().into_iter().collect();
+        let other_set: HashSet<Point> = other.0.clone().into_iter().collect();
 
-    points1
-        .intersection(&points2)
-        .map(|point| point.clone())
-        .collect()
-}
+        self_set
+            .intersection(&other_set)
+            .map(|point| *point)
+            .collect()
+    }
 
-fn calculate_manhattan_distance(point: &Point) -> i32 {
-    point.x + point.y
-}
-
-pub fn calculate_manhattan_distance_for_directions(
-    wire1_directions: Vec<Direction>,
-    wire2_directions: Vec<Direction>,
-) -> i32 {
-    let wire1 = calculate_wire(wire1_directions);
-    let wire2 = calculate_wire(wire2_directions);
-    let intersections = find_wire_intersections(wire1, wire2);
-
-    calculate_manhattan_distance(
-        intersections
+    fn nearest_intersection(&self, other: &Wire) -> Point {
+        *self
+            .intersections(other)
             .iter()
             .filter(|point| point.x >= 0 && point.y >= 0)
             .min()
-            .expect("Should be at least one intersection"),
-    )
+            .expect("Should be at least one intersection")
+    }
+
+    fn steps(&self, selected: &Point) -> usize {
+        1 + self
+            .0
+            .iter()
+            .position(|&point| point == *selected)
+            .expect(&format!(
+                "Point {:?} should exist on Wire {:?}",
+                selected, self
+            ))
+    }
+
+    pub fn total_steps_for_nearest_intersection(&self, other: &Wire) -> usize {
+        let intersection = self
+            .intersections(other)
+            .into_iter()
+            .min_by(|x, y| (self.steps(x) + other.steps(x)).cmp(&(self.steps(y) + other.steps(y))))
+            .expect("There should be at least one intersection");
+
+        self.steps(&intersection) + other.steps(&intersection)
+    }
+}
+
+pub fn manhattan_distance_for_directions_string(
+    wire1_directions: &str,
+    wire2_directions: &str,
+) -> i32 {
+    let wire1 = Wire::from_directions_string(wire1_directions);
+    let wire2 = Wire::from_directions_string(wire2_directions);
+
+    wire1.nearest_intersection(&wire2).manhattan_distance()
 }
 
 #[cfg(test)]
 mod tests {
     use super::Direction::*;
-    use super::{
-        calculate_manhattan_distance, calculate_manhattan_distance_for_directions, calculate_wire,
-        find_wire_intersections, Direction,
-    };
+    use super::{manhattan_distance_for_directions_string, Direction, Point, Wire};
 
     #[test]
     fn test_direction_from_line() {
         assert_eq!(
             vec![Right(8), Up(5), Left(5), Down(3)].as_slice(),
-            Direction::from_line(String::from("R8,U5,L5,D3\n")).as_slice(),
+            Direction::from_line("R8,U5,L5,D3\n").as_slice(),
         );
         assert_eq!(
             vec![Up(7), Right(6), Down(4), Left(4)].as_slice(),
-            Direction::from_line(String::from("U7,R6,D4,L4\n")).as_slice(),
+            Direction::from_line("U7,R6,D4,L4\n").as_slice(),
         );
     }
 
     #[test]
-    fn test_calculate_manhattan_distance() {
-        let wire1 = calculate_wire(vec![Right(8), Up(5), Left(5), Down(3)]);
-        let wire2 = calculate_wire(vec![Up(7), Right(6), Down(4), Left(4)]);
-        let intersections = find_wire_intersections(wire1, wire2);
+    fn test_manhattan_distance() {
+        let wire1 = Wire::from_directions(vec![Right(8), Up(5), Left(5), Down(3)]);
+        let wire2 = Wire::from_directions(vec![Up(7), Right(6), Down(4), Left(4)]);
+        let intersections = wire1.intersections(&wire2);
         assert_eq!(2, intersections.len());
-        assert_eq!(
-            6,
-            calculate_manhattan_distance(
-                intersections
-                    .iter()
-                    .min()
-                    .expect("Should be a minimum intersection")
-            )
-        );
+        assert_eq!(6, Point { x: 3, y: 3 }.manhattan_distance());
     }
 
     #[test]
-    fn test_calculate_manhattan_distance_for_directions() {
+    fn test_manhattan_distance_for_directions() {
         assert_eq!(
             6,
-            calculate_manhattan_distance_for_directions(
-                vec![Right(8), Up(5), Left(5), Down(3)],
-                vec![Up(7), Right(6), Down(4), Left(4)]
-            )
+            manhattan_distance_for_directions_string("R8,U5,L5,D3\n", "U7,R6,D4,L4\n")
         );
         assert_eq!(
             159,
-            calculate_manhattan_distance_for_directions(
-                vec![
-                    Right(75),
-                    Down(30),
-                    Right(83),
-                    Up(83),
-                    Left(12),
-                    Down(49),
-                    Right(71),
-                    Up(7),
-                    Left(72)
-                ],
-                vec![
-                    Up(62),
-                    Right(66),
-                    Up(55),
-                    Right(34),
-                    Down(71),
-                    Right(55),
-                    Down(58),
-                    Right(83)
-                ]
+            manhattan_distance_for_directions_string(
+                "R75,D30,R83,U83,L12,D49,R71,U7,L72",
+                "U62,R66,U55,R34,D71,R55,D58,R83"
             )
         );
         assert_eq!(
             135,
-            calculate_manhattan_distance_for_directions(
-                vec![
-                    Right(98),
-                    Up(47),
-                    Right(26),
-                    Down(63),
-                    Right(33),
-                    Up(87),
-                    Left(62),
-                    Down(20),
-                    Right(33),
-                    Up(53),
-                    Right(51)
-                ],
-                vec![
-                    Up(98),
-                    Right(91),
-                    Down(20),
-                    Right(16),
-                    Down(67),
-                    Right(40),
-                    Up(7),
-                    Right(15),
-                    Up(6),
-                    Right(7)
-                ]
+            manhattan_distance_for_directions_string(
+                "R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51",
+                "U98,R91,D20,R16,D67,R40,U7,R15,U6,R7"
             )
+        );
+    }
+
+    #[test]
+    fn test_total_steps_for_nearest_intersection() {
+        let wire1 = Wire::from_directions(vec![Right(8), Up(5), Left(5), Down(3)]);
+        let wire2 = Wire::from_directions(vec![Up(7), Right(6), Down(4), Left(4)]);
+
+        assert_eq!(30, wire1.total_steps_for_nearest_intersection(&wire2));
+
+        assert_eq!(
+            610,
+            Wire::from_directions_string("R75,D30,R83,U83,L12,D49,R71,U7,L72")
+                .total_steps_for_nearest_intersection(&Wire::from_directions_string(
+                    "U62,R66,U55,R34,D71,R55,D58,R83"
+                ))
+        );
+
+        assert_eq!(
+            410,
+            Wire::from_directions_string("R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51")
+                .total_steps_for_nearest_intersection(&Wire::from_directions_string(
+                    "U98,R91,D20,R16,D67,R40,U7,R15,U6,R7"
+                ))
         );
     }
 }
--- rust/src/main.rs
+++ rust/src/main.rs
@@ -2,7 +2,7 @@ mod day1;
 mod day2;
 mod day3;
 
-use day3::{calculate_manhattan_distance_for_directions, Direction};
+use day3::Wire;
 use std::io;
 
 fn main() -> io::Result<()> {
@@ -10,11 +10,10 @@ fn main() -> io::Result<()> {
     let mut line1 = String::new();
     let mut line2 = String::new();
     if stdin.read_line(&mut line1).is_ok() && stdin.read_line(&mut line2).is_ok() {
-        let distance = calculate_manhattan_distance_for_directions(
-            Direction::from_line(line1),
-            Direction::from_line(line2),
-        );
-        println!("Distance: {}", distance);
+        let wire1 = Wire::from_directions_string(&line1);
+        let wire2 = Wire::from_directions_string(&line2);
+        let steps = wire1.total_steps_for_nearest_intersection(&wire2);
+        println!("Steps: {}", steps);
     }
     Ok(())
 }
